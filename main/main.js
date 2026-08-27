2cd0906acd10e095ffee49e0f1c9e51b6185375c
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-08-27 22:29:26 +0700

    test

--- main/main.js
+++ main/main.js
@@ -3,9 +3,11 @@ import { sleep, scenario, error_check, options } from '../config/common.js';
 export { options }; const cid = __ENV.cid || '1'; let response;
 import { DownloadFile, GetProfile, PostProfile, PostProfile_2, PostProfile_3, UploadFile } from '../api/example.js';
 import { dispenses } from '../api/dispenses.js';
+import { questionnaire } from '../api/questionnaire.js';
 //============================================================================
 export default function () {    //เรียกใช้ API ใน export default function
-  response = dispenses(cid)  // เปลี่ยน cid ก่อนยิงทุกครั้ง (บรรทัด 13 ที่ไฟล์ open.sh)
+  //response = dispenses(cid)  // เปลี่ยน cid ก่อนยิงทุกครั้ง (บรรทัด 13 ที่ไฟล์ open.sh)
+  response = questionnaire()
   error_check(response);
   sleep(1)
 }
